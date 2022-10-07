030e24d9ce4dc6fe21868d7d067511f4419d4028
Harvey Unsaac <193837@unsaac.edu.pe>
2022-10-08 00:11:35 +0200

    :sparkles: Add hashes, minimizer and compression

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
@@ -44,7 +46,11 @@ module.exports = {
         template: './public/index.html',
         filename: './index.html'
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin(
+            {
+                filename: 'assets/[name].[contenthash].css'
+            }
+        ),
         new CopyPlugin({
             patterns: [
                 {
@@ -53,5 +59,12 @@ module.exports = {
                 }
             ]
         })
-    ]
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    }
  }
